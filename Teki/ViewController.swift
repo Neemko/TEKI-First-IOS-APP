5f6aa15765aa8e47e3e2b77a740dcec0e02f06f6
Neemko <thomascapo31@gmail.com>
2020-09-28 19:27:43 +0200

    Création de l'application TEKI

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le KArl Lagarfeld", "la Scarlett Johansson"]
+    var activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBAction func changeQuote() {
+        var randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        var randomCelebrity = celebrities[randomIndex1]
+        print(randomCelebrity)
+        
+        var randomIndex2 = Int(arc4random_uniform(UInt32(activities.count)))
+        var randomActivity = activities[randomIndex2]
+        print(randomActivity)
+        
+        quoteLabel.text = "Le texte est modifié !"
     }
-
-
 }
 
